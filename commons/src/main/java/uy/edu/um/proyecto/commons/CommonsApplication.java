63b09beb4af4cd43925ee5a4df46e634dc177845
geropo08 <84402339+geropo08@users.noreply.github.com>
2023-10-13 13:46:31 -0300

    camb

--- commons/src/main/java/uy/edu/um/proyecto/commons/CommonsApplication.java
+++ commons/src/main/java/uy/edu/um/proyecto/commons/CommonsApplication.java
@@ -1,13 +1,10 @@
 package uy.edu.um.proyecto.commons;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class CommonsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonsApplication.class, args);
+		System.out.println("Hola");
 	}
 
 }
